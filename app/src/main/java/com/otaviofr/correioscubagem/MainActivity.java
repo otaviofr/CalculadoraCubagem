f900ca93a7976a7d39f91152b2c1a4ceb8842515
Otávio Freitas <otavio.freitasb@gmail.com>
2019-08-19 10:03:46 -0300

    botao calcular

--- app/src/main/java/com/otaviofr/correioscubagem/MainActivity.java
+++ app/src/main/java/com/otaviofr/correioscubagem/MainActivity.java
@@ -3,8 +3,13 @@ package com.otaviofr.correioscubagem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -14,6 +19,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.mViewHolder.textResultado = findViewById(R.id.text_View_Reultado);
+        this.mViewHolder.editAltura = findViewById(R.id.edit_altura);
+        this.mViewHolder.editLargura = findViewById(R.id.edit_largura);
+        this.mViewHolder.editComprimento = findViewById(R.id.edit_comprimento);
+        this.mViewHolder.btnCalcular = findViewById(R.id.button_calcular);
+        this.mViewHolder.btnNovo = findViewById(R.id.button_novo);
+
+        this.mViewHolder.btnNovo.setOnClickListener(this);
+        this.mViewHolder.btnCalcular.setOnClickListener(this);
+
+        this.LimpaValores();
     }
+
+
+    @Override
+    public void onClick(View v) {
+        //Botão Calcular
+        if(v.getId() == R.id.button_calcular){
+            String valorAltura = this.mViewHolder.editAltura.getText().toString();
+            if("".equals(valorAltura)){
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if(v.getId() == R.id.button_calcular){
+            String valorLargura = this.mViewHolder.editLargura.getText().toString();
+            if("".equals(valorLargura)){
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if(v.getId() == R.id.button_calcular){
+            String valorComprimento = this.mViewHolder.editComprimento.getText().toString();
+            if("".equals(valorComprimento)){
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+
+        }
+
+        //Botão Novo
+        if (v.getId() == R.id.button_novo) {
+           this.mViewHolder.editAltura.setText("");
+            this.mViewHolder.editLargura.setText("");
+            this.mViewHolder.editComprimento.setText("");
+        }
+
+    }
+
+    private void LimpaValores() {
+        this.mViewHolder.textResultado.setText("");
+    }
+
+    private static class ViewHolder {
+        EditText editAltura;
+        EditText editLargura;
+        EditText editComprimento;
+        Button btnCalcular;
+        Button btnNovo;
+        TextView textResultado;
+    }
+
 
 }
